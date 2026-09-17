886c67deca394d978e46d2e54bebbcb5496320f1
Adi-node <adityap1364@gmail.com>
2026-09-17 11:35:04 +0530

    built managers and completed index file

---
+++ backend/src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("message", (message) => {
+    console.log("Message:", message);
+
+    // Send message to everyone
+    io.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
